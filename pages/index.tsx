f50444eefd8f460c968d81bdfd7ea26f44b89cc6
leesh9098 <leesh9098@naver.com>
2022-08-18 15:45:05 +0900

    modify text

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,14 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <h1>
-      This is deploy test with domain.
-      2022.08.18 automatically deployed with github action and AWS
-    </h1>
+    <>
+      <h1>
+        This is deploy test with domain.
+      </h1>
+      <h1>
+        2022.08.18 automatically deployed with github action and AWS
+      </h1>
+    </>
   )
 }
 
