299efde69413670752b7a7e921a30907e077f3ec
leesh9098 <leesh9098@naver.com>
2022-11-09 14:14:27 +0900

    build test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,6 @@
 import type { AppProps } from 'next/app';
 import GlobalStyles from '../styles/GlobalStyles';
 import { AmplifyProvider } from "@aws-amplify/ui-react";
-// import awsconfig from "../src/aws-exports";
-// import { Amplify } from "aws-amplify";
-
-// Amplify.configure(awsconfig);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
