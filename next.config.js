85315e0cb732620ad53c6d315faf5e06dfc2b527
leesh9098 <leesh9098@naver.com>
2022-11-09 17:52:35 +0900

    downgrade next and react version

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
+  webpack5: false,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    })
+
+    return config
+  }
 }
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import type { AppProps } from 'next/app';
 import GlobalStyles from '../styles/GlobalStyles';
 import { AmplifyProvider } from "@aws-amplify/ui-react";
+import awsmobile from "../src/aws-exports";
+import { Amplify } from "aws-amplify";
+
+Amplify.configure(awsmobile);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
