f68e9de59c6a58b3d8847de220e44ab7ddf04ccd
leesh9098 <leesh9098@naver.com>
2022-11-14 13:16:05 +0900

    add get data from dynamodb test

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,7 +32,7 @@ export default function Home({ serverTime }: Props) {
         2022.09.13 ECS Deploy Test
       </h1>
       <h1>Now : {serverTime.serverTime}</h1>
-      <button>Get Dynamodb's Item</button>
+      <button onClick={getData}>Get Dynamodb's Item</button>
       <b>{data}</b>
     </>
   )
