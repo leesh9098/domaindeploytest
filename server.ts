3ebd7c239327fd79fd714f2335a79e2b1f884922
leesh9098 <leesh9098@naver.com>
2022-11-14 11:54:43 +0900

    edit Dockerfile WORKDIR

---
+++ server.ts
@@ -0,0 +1,20 @@
+// import { NextConfig } from "next";
+// import { NextServer } from "next/dist/server/next";
+// import { config } from "./.next/required-server-files.json";
+// import serverless from "serverless-http";
+
+// const nextServer = new NextServer({
+//     hostname: "localhost",
+//     port: 5000,
+//     dir: __dirname,
+//     dev: false,
+//     conf: {
+//         ...(config as NextConfig)
+//     }
+// })
+
+// export const handler = serverless(nextServer.getRequestHandler(), {
+//     binary: ["*/*"]
+// })
+
+export const handler = ""
